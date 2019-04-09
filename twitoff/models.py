aaeff83265ec0d1d0cbf88eea03ab65760cd5df4
tesseract314 <tesseract314@users.noreply.github.com>
2019-04-09 15:48:42 -0700

    add template, update models, connect to APIs

--- twitoff/models.py
+++ twitoff/models.py
@@ -1,23 +1,25 @@
-"""SQLAlchemy models for TwitOff"""
-
+"""SQLAlchemy models and utility functions for TwitOff."""
 from flask_sqlalchemy import SQLAlchemy
 
 DB = SQLAlchemy()
 
 class User(DB.Model):
-	"""Twitter users that we pull and analyze tweets for."""
-	id = DB.Column(DB.BigInteger, primary_key=True)
-	name = DB.Column(DB.String(15), nullable=False)
+    """Twitter users corresponding to Tweets in the Tweet table."""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    name = DB.Column(DB.String(15), nullable=False)
+    # Tweet IDs are ordinal ints, so can be used to fetch only more recent
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
-	def __repr__(self):
-		return '<User {}>'.format(self.name)
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
 
 class Tweet(DB.Model):
-	""" Tweets."""
-	id = DB.Column(DB.BigInteger, primary_key=True)
-	text = DB.Column(DB.Unicode(280))
-	user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
-	user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+    """Tweets and their embeddings from Basilica."""
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(300))  # Allowing for full + links
+    embedding = DB.Column(DB.PickleType, nullable=False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
-	def __repr__(self):
-		return '<Tweet {}>'.format(self.text)
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
